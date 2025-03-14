545e4bfbf1b3326f1c117d1d40aee82d1edb30e4
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-03-14 22:09:31 +0530

    FAT12 C impl 4

--- src/tools/fat/fat.c
+++ src/tools/fat/fat.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
 
 struct {
     uint8_t BootJumpInstruction[3];
     uint8_t OemIdentifier[8];
-bdb_oem							db 'MSWIN4.1'			; 8 bytes
-bdb_bytes_per_sector: 			dw 512
-bdb_sectors_per_cluster: 		db 1
-bdb_reserved_sectors: 			dw 1
-bdb_fat_count: 					db 2
-bdb_dir_entries_count: 			dw 0E0h
-bdb_total_sectors: 				dw 2880					; 2880 * 512 = 1.44 MB
-bdb_media_descriptor_type: 		db 0F0h					; F0 = 3.5" floppy disk
-bdb_sectors_per_fat: 			dw 9
-bdb_sectors_per_track:			dw 18
-bdb_heads: 						dw 2
-bdb_hidden_sectors: 			dd 0
-bdb_large_sector_count:			dd 0
+    uint16_t BytesPerSector;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
 
-; extended boot record
-ebr_drive_number: 				db 0					; 0x00 = floppy, 0x80 = hdd, useless
-								db 0					; reserved 
-ebr_signature:					db 29h
-ebr_volume_id: 					db 12h, 34h, 56h, 78h 	; serial number, value doesn't matter
-ebr_volume_label:				db 'GWNX OS    '		; 11 bytes, padded with spaces
-ebr_system_id:					db 'FAT 12  '			; 8 bytes
+    // extended boot record
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;                  // serial number, value doesn't matter
+    uint8_t VolumeLabel[11];            // 11 bytes, padded with spaces
+    uint8_t SystemId[8];
 } BootSector;
+
+BootSector g_BootSector;
+
+bool readBootSector(FILE* disk) {
+    return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0;
+}
 
 int main(int argc, char** argv) {
     if (argc < 3) {

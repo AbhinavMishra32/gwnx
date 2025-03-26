93245b70cb93ef406f7190be01cd5b95cb04c749
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-03-26 11:11:28 +0530

    IDT MORE

--- gwny/src/grub/include/util.h
+++ gwny/src/grub/include/util.h
@@ -1,4 +1,4 @@
 #include "stdint.h"
 
 void memset(void *dest, char val, uint32_t count);
-
+void outb(uint16_t port, uint8_t value){
--- gwny/src/grub/src/interrupts/idt.c
+++ gwny/src/grub/src/interrupts/idt.c
@@ -14,6 +14,24 @@ void init_idt(){
 
     memset(&idt_entries, 0, sizeof(struct idt_entry_struct) * 256);
 
+    // 0x20 is for commands and 0x21 data
+    // 0xA0 commands and 0xA1 data
+
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+
 void set_idt_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags){
 
 }
